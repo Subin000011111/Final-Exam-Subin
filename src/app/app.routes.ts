85006dec1b3c4484340192bba039a1371842aa7d
Subin000011111 <subinsamuel104@gmail.com>
2025-04-15 10:48:15 -0400

    Configure routing and navigation for POS system

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
+import { CartSummaryComponent } from './components/cart-summary/cart-summary.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: ProductListComponent },
+  { path: 'cart', component: ShoppingCartComponent },
+  { path: 'summary', component: CartSummaryComponent }
+];
